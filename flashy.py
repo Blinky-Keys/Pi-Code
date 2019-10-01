5b176b5d10f5b52c3a68aec1dc5406b8fe4c5668
Nathan <nathan.ipadman@gmail.com>
2019-10-01 13:27:45 +1300

    Added safe exit for flashy.py

--- flashy.py
+++ flashy.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-
+import sys
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -12,17 +12,23 @@ GPIO.setup(25, GPIO.OUT)
 
 while(1):
 
-	# Uncomment print statements if you want to be annoying lol
-	#print "Lights on"
-	GPIO.output(18, GPIO.HIGH)
-	GPIO.output(23, GPIO.HIGH)
-	GPIO.output(24, GPIO.HIGH)
-	GPIO.output(25, GPIO.HIGH)
-	time.sleep(0.05)
+	try:
+		# Uncomment print statements if you want to be annoying lol
+		#print "Lights on"
+		GPIO.output(18, GPIO.HIGH)
+		GPIO.output(23, GPIO.HIGH)
+		GPIO.output(24, GPIO.HIGH)
+		GPIO.output(25, GPIO.HIGH)
+		time.sleep(0.05)
 
-	#print "Lights off"
-	GPIO.output(18, GPIO.LOW)
-	GPIO.output(23, GPIO.LOW)
-	GPIO.output(24, GPIO.LOW)
-	GPIO.output(25, GPIO.LOW)
-	time.sleep(0.05)
+		#print "Lights off"
+		GPIO.output(18, GPIO.LOW)
+		GPIO.output(23, GPIO.LOW)
+		GPIO.output(24, GPIO.LOW)
+		GPIO.output(25, GPIO.LOW)
+		time.sleep(0.05)
+
+
+	# Exit the program if the user presses CTRL+C
+	except KeyboardInterrupt:
+		sys.exit()
