72b65a6d370baca212f91fafc2b8b1412ffe34f3
Nathan <nathan.ipadman@gmail.com>
2019-10-01 13:17:00 +1300

    Minor annoying change

--- flashy.py
+++ flashy.py
@@ -11,12 +11,16 @@ GPIO.setup(24, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
 
 while(1):
+
+	# Uncomment print statements if you want to be annoying lol
+	#print "Lights on"
 	GPIO.output(18, GPIO.HIGH)
 	GPIO.output(23, GPIO.HIGH)
 	GPIO.output(24, GPIO.HIGH)
 	GPIO.output(25, GPIO.HIGH)
 	time.sleep(0.05)
 
+	#print "Lights off"
 	GPIO.output(18, GPIO.LOW)
 	GPIO.output(23, GPIO.LOW)
 	GPIO.output(24, GPIO.LOW)
